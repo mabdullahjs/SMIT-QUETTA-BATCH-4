6ba82c7c2b3f360d85baae803734e67eb22e26b0
mabdullahjs <mabdullah2037@outlook.com>
2025-11-08 11:26:33 +0500

    introduction to js

---
+++ 02-Javascript/01-Introduction/app.js
@@ -0,0 +1,130 @@
+// console.log("hello world");
+// console.log("second console");
+
+/*debugging
+sdasdd*/
+
+// asdasd
+// fasda
+
+
+
+// console.log("Pakistan Zindabad");
+
+// () function
+
+
+// console.log("hello sapna");
+
+
+// data types
+
+// string "eeaa" 'fdsfsdf' "12$%%$^%$&#&("
+// number 123 43.34
+// boolean true false
+
+// object
+// undefined
+// null
+// bigint
+// symbol
+
+
+
+
+
+
+// console.log("hello string");
+// console.log(23.23);
+// console.log("213");
+
+// console.log(true);
+
+
+
+// container 
+
+
+// var greeting = "hello world";
+// console.log(greeting);
+
+
+// var $_username123 = "Abdullah"
+
+
+// var username = "abdullah";
+// var age = 21;
+// var isLoggedIn = true
+
+// console.log(isLoggedIn);
+
+
+
+// var username = "Abdullah";
+// console.log(username);
+// username = "Ammar"; // reassign
+// console.log(username);
+
+
+// var username = "abdullah";
+// var username = "Usman"; // redeclare
+
+// console.log(username);
+
+
+// console.log() kisi bhi cheez ko console ma print krwana hoto
+// variable (means aik container kaa undar data store krna)
+// data types (string , number, boolean)
+// () function
+// redeclare
+// reassign
+
+
+
+
+// alert and prompt
+
+// alert("hello world")
+// var username = prompt("enter your name");
+// console.log(username);
+
+
+
+// concatination
+
+// console.log(20 + 21);
+// console.log("Muhammad " + "Abdullah " + true);
+// console.log("hello" + 21);
+
+
+// var username = prompt("enter your name")
+// var age = prompt("enter your age")
+
+
+// console.log("username is " + username + " user age is " + age);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
