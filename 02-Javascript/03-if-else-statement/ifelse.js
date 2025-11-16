51244e68da5b56e0b0eea94a653eadcc32c0afce
mabdullahjs <mabdullah2037@outlook.com>
2025-11-16 12:24:40 +0500

    if else

---
+++ 02-Javascript/03-if-else-statement/ifelse.js
@@ -0,0 +1,226 @@
+// console.log("hello world");
+
+// let age = 8;
+
+// // gym admission.
+// // block / scope
+
+// if(age >= 18){
+//     console.log("welcome to my gym.");
+
+// } else {
+//     console.log("dafa hojao.");
+
+// }
+
+// 18 >= gym admission miljayge
+// 15 ~ 17 1000 zyada deka admission milaiga
+// 15 < to admission nahi milaigi.
+
+// let age = 20;
+
+// if (age >= 18) {
+//   console.log("gym admission miljayge");
+// } else if (age >= 15) {
+//   console.log("1000 zyada deka admission milaiga");
+// } else {
+//   console.log("admission nahi milaigi.");
+// }
+
+// var city = prompt("enter city name.");
+
+// if(city === "Karachi"){
+//     console.log("welcome to the city of lights.");
+
+// }else {
+//     console.log("tum bekaar shehar ma rehta ho.");
+
+// }
+
+// var gender = prompt("enter your gender");
+
+// if(gender === "male"){
+//     console.log("Welcome SIR,,");
+
+// } else if(gender === "female") {
+//     console.log("Welcome Unti,,");
+
+// } else {
+//     console.log("kon ho tum????");
+
+// }
+
+// let color = "yellow";
+
+// if(color === "red"){
+//     console.log("Must Stop");
+
+// } else if (color === "green"){
+//     console.log("Move now");
+
+// } else if(color === "yellow"){
+//     console.log("Ready to move");
+
+// } else {
+//     console.log("aba konsa signal pa khara ha?");
+
+// }
+
+// let currentFuel = 10;
+
+// if(currentFuel < 0.25){
+//     console.log("Please refill the fuel in your car");
+
+// } else {
+//     console.log("barabar haa fuel.");
+
+// }
+
+// var c = 12;
+// if (c++ === 13) {
+//   alert("condition 1 is true"); // false
+// }
+// if (c === 13) {
+//   alert("condition 2 is true"); // true
+// }
+// if (++c < 14) {
+//   alert("condition 3 is true"); // false
+// }
+// if (c === 14) {
+//   alert("condition 4 is true"); // true
+// }
+
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost) {
+//   alert("The cost equals");
+// }
+
+// if (true) {
+//   alert("True");
+// }
+// if (false) {
+//   alert("False");
+// }
+
+
+
+
+// let subOne = +prompt("enter marks");
+// let subTwo = +prompt("enter marks");
+// let subThree = +prompt("enter marks");
+
+// let totalMarks = 300;
+// let obtainedMarks = subOne + subTwo + subThree;
+// let percentage = obtainedMarks / totalMarks * 100;
+// console.log("percentage ===> " , percentage + "%");
+
+
+// if(percentage >= 80){
+//     console.log("Grade ==> A+");
+//     console.log("Remarks ==> Excellent");
+    
+// } else if(percentage >= 70){
+//     console.log("Grade ==> A");
+//     console.log("Remarks ==> Good");
+
+// } else if(percentage >= 60){
+//     console.log("Grade ==> B");
+//     console.log("Remarks ==> You need to improve");
+
+// } else {
+//     console.log("Rickshaw chalao...");
+    
+// }
+
+
+// let guessNumber = 7;
+// let userGuess = +prompt("Batao batao kiya likha haa..");
+
+// if(guessNumber === userGuess){
+//     console.log("Bingo! Correct answer");
+    
+// } else if(guessNumber + 1 === userGuess) {
+//     console.log("Qareeb Qareeb hoo..");
+    
+// } else {
+//     console.log("Dafa hojao...");
+    
+// }
+
+
+
+// let num = 30;
+
+// if(num % 3 === 0){
+//     console.log("divisible haa");
+    
+// } else {
+//     console.log("divisible nahi ha.");
+    
+// }
+
+
+
+
+
+// let num = +prompt("enter number");
+
+// if(num % 2 === 0){
+//     console.log("even number");
+    
+// } else {
+//     console.log("odd number");
+    
+// }
+
+
+
+
+
+
+// let currentTemperature = 10;
+
+// if(currentTemperature >= 40){
+//     console.log("Toooo Hot!!");
+    
+// } else if(currentTemperature >= 30){
+//     console.log("The Weather today is Normal.");
+    
+// } else if(currentTemperature >= 20){
+//     console.log("Today’s Weather is cool.");
+    
+// } else{
+//     console.log("OMG! Today’s weather is so Cool.");
+    
+// }
+
+
+
+
+
+// let num1 = +prompt("enter number one");
+// let num2 = +prompt("enter number two");
+// let operator = prompt("enter Operation (+, -, *, /, %)");
+
+
+// if(operator === "+"){
+//     console.log(num1 + num2);
+    
+// } else if(operator === "-"){
+//     console.log(num1 - num2);
+    
+// } else if(operator === "*"){
+//     console.log(num1 * num2);
+    
+// }else if(operator === "/"){
+//     console.log(num1 / num2);
+    
+// }else if(operator === "%"){
+//     console.log(num1 % num2);
+    
+// } else {
+//     console.log("beta kiya chah rha ho?");
+    
+// }
