e43c64c22948bddc079779fd73653437b86dec5d
mabdullahjs <mabdullah2037@outlook.com>
2025-11-23 10:55:27 +0500

    functions completed

---
+++ 02-Javascript/04-Function-&-DOM-intro/function.js
@@ -0,0 +1,110 @@
+// alert()
+// prompt()
+// console.log();
+
+// function reusableCode() {
+//   console.log("hello world 1");
+//   console.log("hello world 2");
+//   console.log("hello world 3");
+//   console.log("hello world 4");
+// }
+
+// reusableCode() // function call krna
+// reusableCode() // function call krna
+// reusableCode() // function call krna
+
+// reuseable function
+// parameter
+// arguments
+
+// function greetUser(username , age){ // para receive krna
+//     console.log("hello " + username + " with age " + age);
+
+// }
+
+// greetUser("abdullah" , 22) // arg send krna
+// greetUser("Usman" , 18)
+
+// function sum(num1 , num2){
+//     console.log("sum of the number is " , num1 + num2);
+
+// }
+
+// sum(20 , 30)
+
+// undefined
+// null
+
+// let username = null;
+// console.log(username);
+
+// function greetUser(username){
+//     console.log("hello " + username);
+// }
+
+// // greetUser("abdullah")
+// // greetUser("Usman")
+// // greetUser("Ammar")
+// // greetUser("Affan")
+// greetUser()
+
+// return
+
+// console.log("hello world");
+
+// function greetUser(username) {
+//     console.log("hello " + username);
+//     return "hello " + username
+
+// }
+
+// let username =  greetUser("abdullah")
+// console.log("username ==> " , username);
+
+// let username = prompt("enter your username");
+// console.log(username);
+
+// js ma return ka matlb jaha func call hua ha waha return hui wi value ajayegi
+// return function ma sirf aik dafa hoga.
+// return ka baad kou bhi piece of code nahi chalaiga.
+
+// function sum(num1 , num2){
+//     return num1 + num2
+// }
+
+// let total = sum(20 , 30)
+// console.log(total);
+
+// console.log(sum(20 , 30));
+// console.log(typeof("abdu"));
+
+// function calculatePercentage(username , num1, num2, num3) {
+//   let totalMarks = 300;
+//   let obtainedMarks = num1 + num2 + num3;
+
+//   let percentage = (obtainedMarks / totalMarks) * 100;
+
+//   return username + " total percentage is " + percentage + "%";
+// }
+
+// console.log(calculatePercentage("abdullah" , 80, 60, 70));
+// console.log(calculatePercentage("Usman" , 70, 70, 90));
+// console.log(calculatePercentage("Ammar" , 100, 90, 90));
+
+
+
+// local variable
+// global variable
+
+let fullName = "Abdullah khan" // global
+function greetUser(){
+    let username = "Abdullah" // local var
+    console.log(username);
+    console.log(fullName);
+    
+}
+// console.log(username); nahi milaiga
+console.log(fullName);
+
+
+greetUser()
