9978cc3fc5d88da073572fbd2f84987e64b0ae54
mabdullahjs <mabdullah2037@outlook.com>
2025-12-06 10:46:47 +0500

    array

---
+++ 02-Javascript/05-Array/script.js
@@ -0,0 +1,98 @@
+// let arr = [12, "hello", true, 45.6, "world", false];
+// console.log(arr[4]);
+// arr[4] = "JavaScript";
+
+// arr.push(13)
+// // console.log(arr)
+
+// arr.pop()
+// // console.log(arr)
+// let fruits = ["apple", "banana", "cherry" , "mango"];
+// // console.log(fruits)
+
+// fruits.unshift("orange")
+// // console.log(fruits)
+// fruits.shift()
+// // console.log(fruits)
+
+// let nums= [1,2,3,4,5,6,7,8,9,10];
+// // console.log(nums.reverse());
+
+// let letters = ['a', 'b', 'c', 'd', 'e','f' ,'g'];
+// // let subArray = letters.slice(1, 4);
+// // console.log(subArray);
+// letters.splice(1,3, 'x', 'y');
+// console.log(letters);
+
+// console.log(letters.length)
+
+// let fruits = ["apple" , "banana" , "orange" , 20 , true]
+
+// let fruits = ["apple" , "banana" , "orange" , "kiwi"];
+
+// // index 0
+// // length hamesha 1 sa start hoti ha.
+
+// console.log(fruits);
+
+// array methods
+
+//push
+//pop
+//unshift
+//shift
+//splice
+//slice
+//split
+//concat
+//sort
+//reverse
+//indexof
+
+let cities = ["karachi", "lahore", "Islamabad", "Quetta", "Hyderabad"];
+
+// cities.push("Sukkur")
+// cities.pop()
+// cities.unshift("ghotki")
+// cities.shift()
+
+// cities.splice(2 , 1)
+// cities.splice(0 , 1)
+// cities.splice(1 , 2)
+// cities.splice(2 , 0 , "Faislabad" , "Sialkot")
+
+// let newArr = cities.slice(1 , 4)
+// console.log(newArr);
+
+// console.log(cities.slice(2 , 4));
+// console.log(cities.slice(0 , 2));
+// console.log(cities.slice(1));
+
+// console.log(cities.slice(1 , 4));
+
+// console.log(cities);
+
+// let username = "Muhammad Abdullah Khan"
+// console.log(username.split("a"));
+
+// let arr1 = [1, 2, 3];
+// let arr2 = [4, 5, 6];
+
+// // console.log(arr1.concat(arr2));
+// console.log(arr2.concat(arr1));
+
+let num = [8, 5, 3, 9, 5, 1, 2, 4];
+// console.log(num.sort());
+// console.log(cities.sort());
+
+
+// let sortNum = num.sort()
+// console.log(sortNum.reverse());
+
+// console.log(cities.indexOf('Quetta'));
+// console.log(cities.indexOf('asdasdasd'));
+
+
+// console.log(cities.includes("Quetta"));
+// console.log(cities.includes("sadasdas"));
+
