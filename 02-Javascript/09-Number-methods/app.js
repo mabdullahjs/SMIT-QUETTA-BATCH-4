dd948e5f2c4ba347f8525a043f36bbf6e33aacad
mabdullahjs <mabdullah2037@outlook.com>
2025-12-28 11:31:07 +0500

    project

--- 02-Javascript/09-Number-methods/app.js
+++ 02-Javascript/09-Number-methods/app.js
@@ -24,29 +24,53 @@ console.log("hello world");
  // 0 masjid
  // 1 chand
 
- const h3 = document.querySelector("#result")
- const img = document.querySelector("#image")
+//  const h3 = document.querySelector("#result")
+//  const img = document.querySelector("#image")
 
-function toss(selected){
-    console.log("user selected ===> " , selected);
-    const randomNumber = Math.floor(Math.random()*2);
+// function toss(selected){
+//     console.log("user selected ===> " , selected);
+//     const randomNumber = Math.floor(Math.random()*2);
+//     console.log(randomNumber);
+    
+
+//     if((randomNumber === 0 && selected === "masjid") || (randomNumber === 1 && selected === "chand")){
+//         console.log("user toss jeet gaya");
+//         h3.innerHTML = "User won the toss"
+        
+//     }else {
+//         console.log("user toss haar gaya");
+//         h3.innerHTML = "User loss the toss"
+        
+//     }
+    
+
+//     if(randomNumber === 1){
+//         img.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfGI2rvg8r5VWyhgnFA7-HT927YCn7qUnefQ&s"
+//     } else {
+//         img.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQORvLB1jcIRSHTE0LoUvTkP45QbFsbHyu_A&s"
+//     }
+// }
+
+
+
+const input = document.querySelector("#number");
+const result = document.querySelector("#result");
+
+function guessTheNumber(){
+    if(input.value > 10 || input.value < 1){
+        alert("bhaijaan 1 - 10 ka darmiyaan number daalo")
+    }
+
+    const userInput = +input.value
+
+    const randomNumber = Math.ceil(Math.random()*10);
     console.log(randomNumber);
     
-
-    if((randomNumber === 0 && selected === "masjid") || (randomNumber === 1 && selected === "chand")){
-        console.log("user toss jeet gaya");
-        h3.innerHTML = "User won the toss"
-        
-    }else {
-        console.log("user toss haar gaya");
-        h3.innerHTML = "User loss the toss"
-        
-    }
-    
-
-    if(randomNumber === 1){
-        img.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfGI2rvg8r5VWyhgnFA7-HT927YCn7qUnefQ&s"
+    if(randomNumber === userInput){
+        result.innerHTML = "You won"
+    } else if(randomNumber === userInput + 1 || randomNumber === userInput - 1) {
+        result.innerHTML = "You are close enough"
     } else {
-        img.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQORvLB1jcIRSHTE0LoUvTkP45QbFsbHyu_A&s"
+        result.innerHTML = "You loss"
     }
 }
