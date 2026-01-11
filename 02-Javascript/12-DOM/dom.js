3e066d09769e4937dc3c64ea98b7a5dbaecab1ad
mabdullahjs <mabdullah2037@outlook.com>
2026-01-11 10:31:26 +0500

    dom

---
+++ 02-Javascript/12-DOM/dom.js
@@ -0,0 +1,44 @@
+// console.log("hello world");
+// document object model
+
+
+// selecting or targeting elements.
+
+// const h1 = document.getElementById("head-one");
+// const h1 = document.querySelector('#head-one')
+// const h1 = document.querySelectorAll(".heading")
+// const h1 = document.getElementsByClassName('heading');
+// const h1 = document.getElementsByTagName('h1');
+
+
+// h1[1].innerHTML = "changedd"
+// console.log(h1);
+
+
+
+// console.log(document.childNodes[1].childNodes[1].childNodes[9].innerHTML);
+
+// const para = document.querySelector(".para");
+// console.log(para.childNodes[0].nodeType);
+
+
+
+// console.log(ul.firstChild);
+// console.log(ul.lastChild.parentNode);
+
+// const ul = document.querySelector("ul");
+// ul.innerHTML = `<li id="li1">Home</li><li id="li2">About</li><li id="li3">Contact</li><li id="li4">Services</li>`
+
+
+// const li1 = document.createElement('LI');
+// const li2 = document.createElement('LI');
+// const li3 = document.createElement('LI');
+// const text1 = document.createTextNode('Home');
+// const text2 = document.createTextNode('About');
+// const text3 = document.createTextNode('Contact');
+// li1.appendChild(text1);
+// li2.appendChild(text2);
+// li3.appendChild(text3)
+// ul.appendChild(li1)
+// ul.appendChild(li2)
+// ul.appendChild(li3)
