65a77fe0d9da8d9ee89ea7162759c509a2b593af
mabdullahjs <mabdullah2037@outlook.com>
2026-03-28 13:54:54 +0500

    UseEffect

--- 03-React/useEffect/src/App.jsx
+++ 03-React/useEffect/src/App.jsx
@@ -1,18 +1,56 @@
 import React, { useEffect, useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0);
+  // const [secCount, setSecCount] = useState(10);
   // useEffect(() => {
-  //   console.log("useEffect chal gia");
-  // })
+  //   console.log("Mount");
+
+  //   return ()=>{
+  //     console.log("UnMount");
+  //   }
+  // }, [count])
+
+
+
+  const [data, setData] = useState(null);
+
   useEffect(() => {
-    console.log("useEffect chal gia");
-  } , [])
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then((res) => {
+        console.log(res);
+        setData(res)
+      })
+      .catch((eror) => {
+        console.log(eror);
+      })
+
+  }, [])
+
 
   return (
     <>
-      <h4>Count:{count}</h4>
+      {/* <h4>Count:{count}</h4>
       <button onClick={() => setCount(count + 1)} >Increase</button>
+
+      <h4>SecCount:{secCount}</h4>
+      <button onClick={() => setSecCount(secCount + 1)} >Increase</button> */}
+
+
+
+      {data?.length ? (
+        data.map(item => (
+          <h1 key={item.id}>{item.name} {item.email}</h1>
+        ))
+      ) : (
+        <h1>Loading..</h1>
+      )}
+
+
+
+
+
     </>
   )
 }
@@ -21,4 +59,7 @@ export default App
 
 // 
 // useEffect  => Hook
+// useEffect ma Dependency array na dy to wo every time run go ga 
+//  useEffect ma Dependency array empty de to wo one time run ho ga
+// useEffect ma Dependency array ma state pas ke to os state ky change per he useEffect chaly ga
 
