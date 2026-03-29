a53e30dcb4a9592fa00e702a08bcad09d2c42a34
mabdullahjs <mabdullah2037@outlook.com>
2026-03-29 13:15:46 +0500

    api handeling

--- 03-React/02-Hooks/src/App.jsx
+++ 03-React/02-Hooks/src/App.jsx
@@ -304,12 +304,41 @@
 // export default App
 
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
+
+  const [loading , setLoading] = useState(true);
+  const [error , setError] = useState(false);
+  const [users , setUsers] = useState(null);
+
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(res => {
+      console.log(res);
+      setUsers(res)
+      
+    }).catch(err => {
+      console.log(err);
+      setError(true)
+      
+    }).finally(()=>{
+      setLoading(false)
+    })
+  } , [])
   return (
     <>
       <h1>Hello world</h1>
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>internal server error</h1>}
+      {/* {users ? users.map(item => {
+        return <h1 key={item.id}>{item.name}</h1>
+      }): null} */}
+
+      {users &&  users.map(item => {
+        return <h1 key={item.id}>{item.name}</h1>
+      })}
     </>
   )
 }
